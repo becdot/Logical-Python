0a218607bf8d1594e1e4e731a6436dc5f013d67c
Becca Liss <becca.liss@gmail.com>
2012-11-19 17:32:00 -0500

    Define functions in terms of nand, add Logic class
    - Overloads &, |, ^ operators by defining them in terms of nand
    - Defines mux and dmux in terms of nand
    - Puts all functions under the Logic class

--- logic.py
+++ logic.py
@@ -1,77 +1,122 @@
-### Test or helper functions
-def test_bit_input(num, bit_len):
-    "Returns true if the number matches its required bit length"
-    if (num == 0 or num == 1) and bit_len != 1:
-        return False
-    if num > 0 and num.bit_length() != bit_len:
-        return False
-    return True
+class Logic:    
 
-def num_to_bin(num):
-    "Returns the binary representation of a number (8 -> '0b1000' or '0b1' -> '0b1')"
-    if 'b' in str(num):
-        return str(num)
-    else:
-        return bin(num)
+    ### Test or helper functions
+    def test_bit_input(self, num, bit_len):
+        "Returns true if the number matches its required bit length"
+        if (num == 0 or num == 1) and bit_len != 1:
+            return False
+        if num > 0 and num.bit_length() != bit_len:
+            return False
+        return True
 
-def num_to_right_bits(num, bits):
-    "Given a number, returns that number as the length of bits specified (2, 4 -> '0b0010')"
-    num = num_to_bin(num)
-    first = num[:num.index('b') + 1]
-    last = num[num.index('b') + 1:]
-    bits_to_add = bits - len(last)
+    # def num_to_bin(self, num):
+    #     "Returns the binary representation of a number (8 -> '0b1000' or '0b1' -> '0b1')"
+    #     if 'b' in str(num):
+    #         return str(num)
+    #     else:
+    #         return bin(num)
 
-    for i in range(bits_to_add):
-        last = '0' + last
-    return first + last
+    # def num_to_right_bits(self, num, bits):
+    #     "Given a number, returns that number as the length of bits specified (2, 4 -> '0b0010')"
+    #     num = num_to_bin(num)
+    #     first = num[:num.index('b') + 1]
+    #     last = num[num.index('b') + 1:]
+    #     bits_to_add = bits - len(last)
+
+    #     for i in range(bits_to_add):
+    #         last = '0' + last
+    #     return first + last
 
 
-### Logic gates
+    ### Logic gates
 
-# Chapter 1
-def nand(a, b):
-    "Returns not(a and b)"
-    assert (test_bit_input(a, 1) and test_bit_input(b, 1))
+    # Chapter 1
+    def nand(self, a, b):
+        "Returns not(a and b)"
+        assert (self.test_bit_input(a, 1) and self.test_bit_input(b, 1))
 
-    return not(a&b)
+        return not(a and b)
 
-def mux(a, b, sel):
-    "If sel = 0, returns a; if sel = 1, returns b"
-    assert (test_bit_input(a, 1) and test_bit_input(b, 1) and test_bit_input(sel, 1))
+    def nott(self, a):
+        "Returns not(a)"
+        return self.nand(a, a)
 
-    return (a and not sel) or (b and sel)
+    def __and__(self, a, b):
+        "Overloads the & operator, defined in terms of nand"
+        c = self.nand(a, b)
+        return self.nand(c, c)
 
-def dmux(input, sel):
-    "if sel = 0, returns (a=input, b=0); if sel = 1, returns (a=0, b=input)"
-    assert (test_bit_input(input, 1) and test_bit_input(sel, 1))
+    def __or__(self, a, b):
+        "Overloads the | operator, defined in terms of nand"
+        x = self.nand(a, a)
+        y = self.nand(b, b)
+        return self.nand(x, y)
 
-    x, y = (input and not sel), (input and sel)
-    return (x, y)
+    def __xor__(self, a, b):
+        "Overloads the ^ operator, defined in terms of nand"
+        notb = self.nand(b, b)
+        nota = self.nand(a, a)
+        a_and_notb = self.nand(self.nand(a, notb), self.nand(a, notb))
+        b_and_nota = self.nand(self.nand(b, nota), self.nand(b, nota))
+        not_aandnotb = self.nand(a_and_notb, a_and_notb)
+        not_bandnota = self.nand(b_and_nota, b_and_nota)
+        return self.nand(not_aandnotb, not_bandnota)
 
-def and16(a, b):
-    "Returns a 16-bit number where out[0] = (a[0] and b[0]), etc.."
-    a, b = int(num_to_bin(a), 2), int(num_to_bin(b), 2)
-    out = a & b
-    return num_to_right_bits(out, 16)
+    def mux(self, a, b, sel):
+        "If sel = 0, returns a; if sel = 1, returns b"
+        assert (self.test_bit_input(a, 1) and self.test_bit_input(b, 1) and self.test_bit_input(sel, 1))
 
-def not16(a):
-    "Returns a 16-bit number where out[0] = not a[0]"
-    a = num_to_bin(a)
-    out = ~int(a, 2)
-    return num_to_right_bits(out, 16)
+        return (a & self.nott(sel) | (b & sel))  
 
-def or16(a, b):
-    "Returns a 16-bit number where out[0] = (a[0] or b[0]), etc.."
-    a, b = int(num_to_bin(a), 2), int(num_to_bin(b), 2)
-    out = a | b
-    return num_to_right_bits(out, 16)
+    def dmux(self, a, sel):
+        "If sel = 0, returns (a=input, b=0); if sel = 1, returns (a=0, b=input)"
+        assert (self.test_bit_input(a, 1) and self.test_bit_input(sel, 1))
 
-def ormultiway(a):
-    "Returns True if any bits in a are 1, else returns False"
-    a = int(num_to_bin(a), 2)
-    if a == 0:
-        return False
-    return True
+        x, y = (a & self.nott(sel), (a & sel))
+        return (x, y)
+
+
+
+
+
+
+    # def mux(a, b, sel):
+    #     "If sel = 0, returns a; if sel = 1, returns b"
+    #     assert (test_bit_input(a, 1) and test_bit_input(b, 1) and test_bit_input(sel, 1))
+
+    #     return (a and not sel) or (b and sel)
+
+    # def dmux(input, sel):
+    #     "if sel = 0, returns (a=input, b=0); if sel = 1, returns (a=0, b=input)"
+    #     assert (test_bit_input(input, 1) and test_bit_input(sel, 1))
+
+    #     x, y = (input and not sel), (input and sel)
+    #     return (x, y)
+
+    # def and16(a, b):
+    #     "Returns a 16-bit number where out[0] = (a[0] and b[0]), etc.."
+    #     a, b = int(num_to_bin(a), 2), int(num_to_bin(b), 2)
+    #     out = a & b
+    #     return num_to_right_bits(out, 16)
+
+    # def not16(a):
+    #     "Returns a 16-bit number where out[0] = not a[0]"
+    #     a = num_to_bin(a)
+    #     out = ~int(a, 2)
+    #     return num_to_right_bits(out, 16)
+
+    # def or16(a, b):
+    #     "Returns a 16-bit number where out[0] = (a[0] or b[0]), etc.."
+    #     a, b = int(num_to_bin(a), 2), int(num_to_bin(b), 2)
+    #     out = a | b
+    #     return num_to_right_bits(out, 16)
+
+    # def ormultiway(a):
+    #     "Returns True if any bits in a are 1, else returns False"
+    #     a = int(num_to_bin(a), 2)
+    #     if a == 0:
+    #         return False
+    #     return True
 
 
 
--- logic_testing.py
+++ logic_testing.py
@@ -1,108 +1,149 @@
 import unittest
 
-from logic import *
+from logic import Logic
 
 
 class test_logic(unittest.TestCase):
 
-    def test_num_to_bin(self):
-        self.assertEquals(num_to_bin(0), '0b0')
-        self.assertEquals(num_to_bin(8), '0b1000')
-        self.assertEquals(num_to_bin(-2), '-0b10')
-        self.assertEquals(num_to_bin('0b1'), '0b1')
-        self.assertEquals(num_to_bin('0b10'), '0b10')
-        self.assertEquals(num_to_bin('-0b01'), '-0b01')
+    # def test_num_to_bin(self):
+    #     self.assertEquals(num_to_bin(0), '0b0')
+    #     self.assertEquals(num_to_bin(8), '0b1000')
+    #     self.assertEquals(num_to_bin(-2), '-0b10')
+    #     self.assertEquals(num_to_bin('0b1'), '0b1')
+    #     self.assertEquals(num_to_bin('0b10'), '0b10')
+    #     self.assertEquals(num_to_bin('-0b01'), '-0b01')
 
-    def test_num_to_bits(self):
-        self.assertEquals(num_to_right_bits(8, 8), '0b00001000')
-        self.assertEquals(num_to_right_bits('0b1000', 8), '0b00001000')
-        self.assertEquals(num_to_right_bits(0, 8), '0b00000000')
-        self.assertEquals(num_to_right_bits('0b0', 4), '0b0000')
-        self.assertEquals(num_to_right_bits(-1, 4), '-0b0001')
-        self.assertEquals(num_to_right_bits('0b01', 4), '0b0001')
-        self.assertEquals(num_to_right_bits(4, 3), '0b100')
-        self.assertEquals(num_to_right_bits('0b100', 3), '0b100')
+    # def test_num_to_right_bits(self):
+    #     self.assertEquals(num_to_right_bits(8, 8), '0b00001000')
+    #     self.assertEquals(num_to_right_bits('0b1000', 8), '0b00001000')
+    #     self.assertEquals(num_to_right_bits(0, 8), '0b00000000')
+    #     self.assertEquals(num_to_right_bits('0b0', 4), '0b0000')
+    #     self.assertEquals(num_to_right_bits(-1, 4), '-0b0001')
+    #     self.assertEquals(num_to_right_bits('0b01', 4), '0b0001')
+    #     self.assertEquals(num_to_right_bits(4, 3), '0b100')
+    #     self.assertEquals(num_to_right_bits('0b100', 3), '0b100')
 
 
     def test_nand(self):
         "Defines a truth table for nand"
-        self.assertTrue(nand(0, 0))
-        self.assertTrue(nand(0, 1))
-        self.assertTrue(nand(1, 0))
-        self.assertFalse(nand(1, 1))
+        x = Logic()
+        self.assertTrue(x.nand(0, 0))
+        self.assertTrue(x.nand(0, 1))
+        self.assertTrue(x.nand(1, 0))
+        self.assertFalse(x.nand(1, 1))
 
     def test_nand_bits(self):
         "nand should only accept 1-bit values for a and b"
-        self.assertRaises(AssertionError, nand, 0, 2)
-        self.assertRaises(AssertionError, nand, 1, 10)
+        x = Logic()
+        self.assertRaises(AssertionError, x.nand, 0, 2)
+        self.assertRaises(AssertionError, x.nand, 1, 10)
+
+    def test_nott(self):
+        "Defines a truth table for not(t)"
+        x = Logic()
+        self.assertTrue(x.nott(0))
+        self.assertFalse(x.nott(1))
+
+    def test___and__(self):
+        "Defines a truth table for and"
+        x = Logic()
+        self.assertFalse(x.__and__(0, 0))
+        self.assertFalse(x.__and__(0, 1))
+        self.assertFalse(x.__and__(1, 0))
+        self.assertTrue(x.__and__(1, 1))
+
+    def test___or__(self):
+        "Defines a truth table for or"
+        x = Logic()
+        self.assertFalse(x.__or__(0, 0))
+        self.assertTrue(x.__or__(0, 1))
+        self.assertTrue(x.__or__(1, 0))
+        self.assertTrue(x.__or__(1, 1))
+
+    def test___xor__(self):
+        "Defines a truth table for xor"
+        x = Logic()
+        self.assertFalse(x.__xor__(0, 0))
+        self.assertTrue(x.__xor__(0, 1))
+        self.assertTrue(x.__xor__(1, 0))
+        self.assertFalse(x.__xor__(1, 1))
 
     def test_mux(self):
         "Defines a truth table for mux"
-        self.assertFalse(mux(0, 0, 0))
-        self.assertFalse(mux(0, 0, 1))
-        self.assertFalse(mux(0, 1, 0))
-        self.assertTrue(mux(0, 1, 1))
-        self.assertTrue(mux(1, 0, 0))
-        self.assertFalse(mux(1, 0, 1))
-        self.assertTrue(mux(1, 1, 0))
-        self.assertTrue(mux(1, 1, 1))
+        x = Logic()
+        self.assertFalse(x.mux(0, 0, 0))
+        self.assertFalse(x.mux(0, 0, 1))
+        self.assertFalse(x.mux(0, 1, 0))
+        self.assertTrue(x.mux(0, 1, 1))
+        self.assertTrue(x.mux(1, 0, 0))
+        self.assertFalse(x.mux(1, 0, 1))
+        self.assertTrue(x.mux(1, 1, 0))
+        self.assertTrue(x.mux(1, 1, 1))
 
     def test_mux_bits(self):
         "mux should only accept 1-bit values for a, b, and sel"
-        self.assertRaises(AssertionError, mux, 0, 1, 2)
-        self.assertRaises(AssertionError, mux, 0, 10, 1)
-        self.assertRaises(AssertionError, mux, 2, 10, 3)
+        x = Logic()
+        self.assertRaises(AssertionError, x.mux, 0, 1, 2)
+        self.assertRaises(AssertionError, x.mux, 0, 10, 1)
+        self.assertRaises(AssertionError, x.mux, 2, 10, 3)
 
     def test_dmux(self):
         "Defines a truth table for dmux"
-        a, b = dmux(0, 0)
+        x = Logic()
+        a, b = x.dmux(0, 0)
         self.assertFalse(a)
         self.assertFalse(b)
-        a, b = dmux(0, 1)
+        a, b = x.dmux(0, 1)
         self.assertFalse(a)
         self.assertFalse(b)
-        a, b = dmux(1, 0)
+        a, b = x.dmux(1, 0)
         self.assertTrue(a)
         self.assertFalse(b)
-        a, b = dmux(1, 1)
+        a, b = x.dmux(1, 1)
         self.assertFalse(a)
         self.assertTrue(b)
 
     def test_dmux_bits(self):
         "dmux should only accept 1-bit values for input and sel"
-        self.assertRaises(AssertionError, dmux, 0, 10)
-        self.assertRaises(AssertionError, dmux, 2, 1)
-        self.assertRaises(AssertionError, dmux, 4, 5)
+        x = Logic()
+        self.assertRaises(AssertionError, x.dmux, 0, 10)
+        self.assertRaises(AssertionError, x.dmux, 2, 1)
+        self.assertRaises(AssertionError, x.dmux, 4, 5)
 
-    def test_and16(self):
-        "Checks that and16(a, b) calculates 'and' bitwise and returns the correct 16-bit number"
-        self.assertEquals(and16(8, 0), '0b0000000000000000')
-        self.assertEquals(and16('0b0000000000001000', -1), '0b0000000000001000')
-        self.assertEquals(and16(3, 1), '0b0000000000000001')
-        self.assertEquals(and16('0b0011', '-0b1100'), '0b0000000000000000')
+    def test_invert(self):
+        x = Logic()
+        self.assertTrue(x.__invert__(0))
+        self.assertFalse(x.__invert__(1))
 
-    def test_not16(self):
-        "Checks that not16(a) calculates 'not' bitwise and returns the correct 16-bit number"
-        self.assertEquals(not16(8), '-0b0000000000001001')
-        self.assertEquals(not16(-1), '0b0000000000000000')
-        self.assertEquals(not16('0b00000000'), '-0b0000000000000001')
-        self.assertEquals(not16('0b0011'), '-0b0000000000000100')
+    # def test_and16(self):
+    #     "Checks that and16(a, b) calculates 'and' bitwise and returns the correct 16-bit number"
+    #     self.assertEquals(and16(8, 0), '0b0000000000000000')
+    #     self.assertEquals(and16('0b0000000000001000', -1), '0b0000000000001000')
+    #     self.assertEquals(and16(3, 1), '0b0000000000000001')
+    #     self.assertEquals(and16('0b0011', '-0b1100'), '0b0000000000000000')
 
-    def test_0r16(self):
-        "Checks that and16(a, b) calculates 'or' bitwise and returns the correct 16-bit number"
-        self.assertEquals(or16(8, 0), '0b0000000000001000')
-        self.assertEquals(or16('0b0000000000001000', -1), '-0b0000000000000001')
-        self.assertEquals(or16(2, 1), '0b0000000000000011')
-        self.assertEquals(or16('0b0011', '-0b0100'), '-0b0000000000000001')  
+    # def test_not16(self):
+    #     "Checks that not16(a) calculates 'not' bitwise and returns the correct 16-bit number"
+    #     self.assertEquals(not16(8), '-0b0000000000001001')
+    #     self.assertEquals(not16(-1), '0b0000000000000000')
+    #     self.assertEquals(not16('0b00000000'), '-0b0000000000000001')
+    #     self.assertEquals(not16('0b0011'), '-0b0000000000000100')
 
-        
-    def test_ormultiway(self):
-        "Checks that the output is correct"
-        self.assertTrue(ormultiway(8))
-        self.assertTrue(ormultiway('0b1000'))
-        self.assertTrue(ormultiway('-0b1'))
-        self.assertFalse(ormultiway(0))
-        self.assertFalse(ormultiway('0b0'))      
+    # def test_0r16(self):
+    #     "Checks that and16(a, b) calculates 'or' bitwise and returns the correct 16-bit number"
+    #     self.assertEquals(or16(8, 0), '0b0000000000001000')
+    #     self.assertEquals(or16('0b0000000000001000', -1), '-0b0000000000000001')
+    #     self.assertEquals(or16(2, 1), '0b0000000000000011')
+    #     self.assertEquals(or16('0b0011', '-0b0100'), '-0b0000000000000001')  
+
+
+    # def test_ormultiway(self):
+    #     "Checks that the output is correct"
+    #     self.assertTrue(ormultiway(8))
+    #     self.assertTrue(ormultiway('0b1000'))
+    #     self.assertTrue(ormultiway('-0b1'))
+    #     self.assertFalse(ormultiway(0))
+    #     self.assertFalse(ormultiway('0b0'))      
 
 
 
